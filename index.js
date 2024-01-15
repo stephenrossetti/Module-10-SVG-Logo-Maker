ccaa795c5c9fdd6f76f97211aef05ea920c76173
Stevie Rossetti <rossetti.stephen@gmail.com>
2024-01-15 17:46:54 -0600

    Added index.js switch and case function. Also changed question since 'new' error kept coming up. Answer was creating a variable that already existed

--- index.js
+++ index.js
@@ -6,3 +6,44 @@ let questions = require('./lib/questions');
 // descructuring the variable to pull in circle/triangle/square features used below //
 let {Circle, Triangle, Square} = require('./lib/shapes');
 
+// Initiate the questions and SVG logo maker//
+
+let generateLogo = () => {
+    inquirer.prompt(questions)
+        .then((data) => {
+            switch (`${data.shape}`) {
+                case 'Circle':
+                    let circle = new Circle(data.text, data.textColor, data.shapeColor)
+                    fs.writeFile("examples/logo.svg", circle.createCircle(), (err) => {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            console.log('Circle logo has been made!');
+                        }
+                    });
+                    break;
+                case 'Triangle':
+                    let triangle = new Triangle(data.text, data.textColor, data.shapeColor)
+                    fs.writeFile("examples/logo.svg", triangle.createTriangle(), (err) => {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            console.log('Triangle logo has been made!');
+                        }
+                    });
+                    break;
+                case 'Square':
+                    let square = new Square(data.text, data.textColor, data.shapeColor)
+                    fs.writeFile("examples/logo.svg", square.createSquare(), (err) => {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            console.log('Square logo has been made!');
+                        }
+                    });
+                    break;
+            }
+        });
+}
+
+generateLogo();
